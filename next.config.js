58a04df0c5ea5a132f3b932fbdd17e5ab4050999
Rishiraj MIshra <rishirajmishraaaa@gmail.com>
2025-10-22 23:29:31 +0000

    studio-4453132615:~/studio{main}$ npm run build
    > nextn@0.1.0 build
    >

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // `output: 'export'` is removed from here
+  experimental: {
+    appDir: true,
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
