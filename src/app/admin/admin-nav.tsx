bdb553575326c25935ca1f76046b75c5962dd7a3
Rishiraj MIshra <rishirajmishraaaa@gmail.com>
2025-10-22 18:20:45 +0000

    admin page khul he nhi rha h bhai

--- src/app/admin/admin-nav.tsx
+++ src/app/admin/admin-nav.tsx
@@ -51,12 +51,12 @@ const navItems = {
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
   React.ComponentPropsWithoutRef<"a"> & { icon: React.ElementType, title: string }
->(({ className, title, children, icon: Icon, ...props }, ref) => {
+>(({ className, title, children, icon: Icon, href, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
         <Link
-          href={props.href || '#'}
+          href={href || '#'}
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
@@ -188,7 +188,7 @@ export default function AdminNav() {
       <NavigationMenu>
           <NavigationMenuList>
               <NavigationMenuItem>
-                  <Link href="/admin" passHref>
+                  <Link href="/admin" legacyBehavior passHref>
                       <NavigationMenuLink active={pathname === '/admin' || pathname === '/admin/dashboard'} className={navigationMenuTriggerStyle()}>
                           Dashboard
                       </NavigationMenuLink>
