0c5fa05e60c46bdaaa92b5cde33d10b1e8d720d0
Rishiraj MIshra <rishirajmishraaaa@gmail.com>
2025-10-23 12:22:30 +0000

    Error adding doctor: FirebaseError: Firebase Storage: No default bucket

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
   "projectId": "cabzi-welr1",
   "appId": "1:786266287419:web:4ad396cbd949ba46695b1e",
   "apiKey": "AIzaSyB8bFxF6fILR4myxGUCN9IR9Qis9ljADMA",
   "authDomain": "cabzi-welr1.firebaseapp.com",
+  "storageBucket": "cabzi-welr1.appspot.com",
   "measurementId": "G-LGK753VG5R",
   "messagingSenderId": "786266287419"
 };
