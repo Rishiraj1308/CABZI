5025b5a116182dca16da78617f087ce123f2c91d
Rishiraj MIshra <rishirajmishraaaa@gmail.com>
2025-10-25 19:30:32 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: Link is

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -2,6 +2,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
