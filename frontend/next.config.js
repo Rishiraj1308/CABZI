02f604afca14311565debba89dff9159e5f85548
Rishiraj MIshra <rishirajmishraaaa@gmail.com>
2025-11-18 07:42:02 +0000

    firebae hosting m deploy kare

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
 
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'placehold.co' },
       { protocol: 'https', hostname: 'picsum.photos' },
